cedd0550ce1374cd10b5adfee4600b1119c64a88
Carlos Herrera <herreradiaz14@gmail.com>
2023-01-25 23:29:53 -0500

    Desafio numero 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { DirectivaGeneralDirective } from './directivas/directiva-general.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    DirectivaGeneralDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/models/estudiante.ts
@@ -0,0 +1,11 @@
+export interface Estudiante{
+  nombre: string;
+  apellido: string;
+  pais: string;
+  sexo: string;
+  foto: string;
+  edad: number;
+  estaMatriculado: boolean;
+  estaActivo: boolean;
+  codigo: string;
+}
